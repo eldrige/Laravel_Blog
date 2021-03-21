efed509a20703ac3c18c0a6427800b7a202f9b78
Apoh Eldrige <peldrige8@gmail.com>
2021-03-21 22:59:06 +0100

    base template setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,34 @@
   <link rel="stylesheet" href={{ 'css/app.css'}}>
 </head>
 
-<body>
+<body class="bg-gray-200">
+  <nav class="p-6 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+      <li>
+        <a class="p-3" href="/">Home</a>
+      </li>
+      <li>
+        <a class="p-3" href="/">Dashboard</a>
+      </li>
+      <li>
+        <a class="p-3" href="/">Posts</a>
+      </li>
+
+      <ul class="flex items-center">
+        <li>
+          <a class="p-3" href="/">Prince Eldrige</a>
+        </li>
+        <li>
+          <a class="p-3" href="/">Login</a>
+        </li>
+        <li>
+          <a class="p-3" href="/">Register</a>
+        </li>
+        <li>
+          <a class="p-3" href="/">Logout</a>
+        </li>
+      </ul>
+  </nav>
   <!-- this injects content to other portions of the app -->
   @yield('content')
 </body>
